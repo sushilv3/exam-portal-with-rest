da4baa442aeae9238ed883d41d6c1c3027660cdc
sushilv3 <sushilv3rma@gmail.com>
2021-07-09 12:05:07 +0530

    removed some imports

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import "./App.css";
+// import "./App.css";
+import ExamApp from "../src/components/exam/ExamApp";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <ExamApp />
         <h1>App component</h1>
       </header>
     </div>
